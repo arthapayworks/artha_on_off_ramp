e7c0e4e2a5c63b5ff8a4737e337622d90a74bc2a
subbareddy <subbareddy@yellowblock.net>
2025-03-26 10:19:06 +0530

    sell

---
+++ src/components/Sell.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Sell: React.FC = () => {
+    return (<div className="p-4 bg-white rounded-lg shadow">
+        <h2 className="text-xl font-semibold mb-4">Sell Crypto</h2>
+    </div>)
+        
+}
+export default Sell;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
+import '../src/index.css'
 export { default as Buy } from './components/Buy'; 
+export { default as Sell } from './components/Sell';
